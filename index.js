03f127b3a37b16b76bf20e66f560a20d03e3592e
acodein <ayushupadhyay373@gmail.com>
2025-10-04 23:04:13 +0530

    feat: Add a random color feature This option colors the boxes with random color and you can clear it too.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const defaultContainer = document.getElementById("default-container");
 const userContainer = document.createElement("div");
 userContainer.id = "user-container";
 
-let userBoxes;
-
-let number = 16;
 const clear = document.querySelector("#clear");
 
 const noOfBoxes = document.querySelector("#box-numbers");
 
+const randomColor = document.querySelector("#random-color");
+
+// DEFAULT GRID
 for (let i = 0; i < 16 * 16; i++) {
   const defaultBoxes = document.createElement("div");
   defaultBoxes.classList.add("box");
@@ -18,7 +18,54 @@ for (let i = 0; i < 16 * 16; i++) {
   defaultBoxes.addEventListener("mouseover", () => {
     defaultBoxes.classList.add("hovered");
   });
+
+  randomColor.addEventListener("click", () => {
+    defaultBoxes.addEventListener("mouseover", () => {
+      defaultBoxes.style.backgroundColor = randomColorGenerator();
+    });
+  });
   clear.addEventListener("click", () => {
     defaultBoxes.classList.remove("hovered");
+    defaultBoxes.style.backgroundColor = "";
   });
 }
+
+// when the user presses Box Numbers for
+//    user required grids from 1 to 100.
+noOfBoxes.addEventListener("click", () => {
+  let number = prompt("Enter the number b/w 1 to 100: ");
+
+  defaultContainer.innerHTML = "";
+
+  for (let i = 0; i < number * number; i++) {
+    const defaultBoxes = document.createElement("div");
+
+    defaultBoxes.classList.add("box");
+
+    defaultContainer.appendChild(defaultBoxes);
+
+    defaultBoxes.addEventListener("mouseover", () => {
+      defaultBoxes.classList.add("hovered");
+    });
+    randomColor.addEventListener("click", () => {
+      defaultBoxes.addEventListener("mouseover", () => {
+        defaultBoxes.style.backgroundColor = randomColorGenerator();
+      });
+    });
+    clear.addEventListener("click", () => {
+      defaultBoxes.classList.remove("hovered");
+      defaultBoxes.style.backgroundColor = "";
+    });
+  }
+});
+
+function randomColorGenerator() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+
+randomColor.addEventListener("click", () => {
+  defaultBoxes.style.backgroundColor = randomColorGenerator();
+});
